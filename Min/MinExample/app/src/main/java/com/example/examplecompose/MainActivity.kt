214884a8fc2e51a847a6180b353165a05453584a
고민균 <97932282+skyblue1232@users.noreply.github.com>
2025-04-09 16:33:14 +0900

    feat/mainActivity

--- Min/MinExample/app/src/main/java/com/example/examplecompose/MainActivity.kt
+++ Min/MinExample/app/src/main/java/com/example/examplecompose/MainActivity.kt
@@ -3,7 +3,6 @@ package com.example.examplecompose
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.activity.enableEdgeToEdge
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Spacer
@@ -11,12 +10,12 @@ import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Slider
+import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
-import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.mutableFloatStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
@@ -30,15 +29,12 @@ import com.example.examplecompose.ui.theme.ExampleComposeTheme
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
         setContent {
             ExampleComposeTheme {
-                Scaffold( modifier = Modifier.fillMaxSize() ) { innerPadding ->
-                    DemoText(
-                        message = "Welcome",
-                        fontSize = 12f,
-                        modifier = Modifier.padding(innerPadding)
-                    )
+                // A surface container using the 'background' color from the theme
+                Surface(
+                    modifier = Modifier.fillMaxSize(),
+                ) {
                     DemoScreen()
                 }
             }
@@ -47,17 +43,47 @@ class MainActivity : ComponentActivity() {
 }
 
 @Composable
-fun DemoText(message: String, fontSize: Float, modifier: Modifier = Modifier) {
+fun DemoScreen() {
+
+    var sliderPosition by remember { mutableFloatStateOf(20f) }
+
+    val handlePositionChange = { position : Float ->
+        sliderPosition = position
+    }
+
+    Column(
+        horizontalAlignment = Alignment.CenterHorizontally,
+        verticalArrangement = Arrangement.Center,
+        modifier = Modifier.fillMaxSize()
+    ) {
+
+        DemoText(message = "Welcome to Compose", fontSize = sliderPosition)
+
+        Spacer(modifier = Modifier.height(150.dp))
+
+        DemoSlider(
+            sliderPosition = sliderPosition,
+            onPositionChange = handlePositionChange
+        )
+
+        Text(
+            style = MaterialTheme.typography.titleLarge,
+            text = sliderPosition.toInt().toString() + "sp"
+        )
+    }
+}
+
+@Composable
+fun DemoText(message: String, fontSize: Float) {
     Text(
         text = message,
         fontSize = fontSize.sp,
-        fontWeight = FontWeight.Bold,
-        modifier = modifier
+        fontWeight = FontWeight.Bold
     )
 }
 
 @Composable
-fun DemoSlider(sliderPosition: Float, onPositionChange: (Float) -> Unit) {
+fun DemoSlider(sliderPosition: Float, onPositionChange: (Float) -> Unit ) {
     Slider(
         modifier = Modifier.padding(10.dp),
         valueRange = 20f..40f,
@@ -66,40 +92,10 @@ fun DemoSlider(sliderPosition: Float, onPositionChange: (Float) -> Unit) {
     )
 }
 
+@Preview(showBackground = true, showSystemUi = true)
 @Composable
-fun DemoScreen() {
-
-    var sliderPosition by remember { mutableStateOf(20f) }
-
-    val handlePositionChange = { position : Float ->
-        sliderPosition = position
-    } // 변수에 새 값을 저장
-
-    Column (
-        horizontalAlignment = Alignment.CenterHorizontally,
-        verticalArrangement = Arrangement.Center,
-        modifier = Modifier.fillMaxSize()
-    ) {
-        DemoText(message = "Welcome", fontSize = sliderPosition)
-
-        Spacer(modifier = Modifier.height(150.dp))
-
-        DemoSlider(sliderPosition = sliderPosition, onPositionChange = handlePositionChange)
-
-        Text(
-            style = MaterialTheme.typography.headlineMedium,
-            text = sliderPosition.toInt().toString() + "sp"
-        )
-    }
-
-} // 컴포즈 런타임 시스템은 이 데이터 변경을 감지하고 사용자 인터페이스를 재구성한다.
-
-@Preview
-@Composable
-fun DemoTextPreview(showBackground: Boolean = true) {
-    DemoText(message = "Welcome", fontSize = 12f)
-    MainActivity().setContent {
+fun Preview() {
+    ExampleComposeTheme {
         DemoScreen()
     }
 }
-
