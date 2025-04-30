2c8481ab4de39f8d40c84010018f8e7635d59560
고민균 <97932282+skyblue1232@users.noreply.github.com>
2025-04-30 14:51:41 +0900

    feat/layout-example

--- Min/MinExample2/app/src/main/java/com/example/minexample2/MainActivity.kt
+++ Min/MinExample2/app/src/main/java/com/example/minexample2/MainActivity.kt
@@ -3,20 +3,29 @@ package com.example.minexample2
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.background
 import androidx.compose.foundation.border
 import androidx.compose.foundation.layout.*
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
+import androidx.compose.material3.TextField
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.layout.Layout
+import androidx.compose.ui.layout.layout
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import androidx.compose.ui.tooling.preview.Preview
+import kotlin.math.roundToInt
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -30,6 +39,9 @@ class MainActivity : ComponentActivity() {
                     MainScreen()
                     MainScreen2()
                     MainBoxScreen()
+                    MainCustomScreen()
+                    MainCascadeScreen()
+                    MainIntrinsicScreen()
                 }
             }
         }
@@ -93,7 +105,6 @@ fun MainBoxScreen() {
         Text("BottomEnd", Modifier.align(Alignment.BottomEnd))
     }
 
-
     // Box(Modifier.size(200.dp).clip(RoundedCornerShape(30.dp)).background(Color.Blue))
 }
 
@@ -128,10 +139,161 @@ fun TextCell(text: String, modifier: Modifier = Modifier) {
         textAlign = TextAlign.Center)
 }
 
+@Composable
+fun MainCustomScreen() {
+    Box(contentAlignment = Alignment.Center,
+        modifier = Modifier.size(120.dp, 80.dp)) {
+        Column {
+            ColorBox(
+                Modifier.exampleLayout(0f).background(Color.Blue)
+            )
+            ColorBox(
+                Modifier.exampleLayout(0.25f).background(Color.Green)
+            )
+            ColorBox(
+                Modifier.exampleLayout(0.5f).background(Color.Yellow)
+            )
+            ColorBox(
+                Modifier.exampleLayout(0.25f).background(Color.Red)
+            )
+            ColorBox(
+                Modifier.exampleLayout(0.0f).background(Color.Magenta)
+            )
+        }
+    }
+}
+
+fun Modifier.exampleLayout(
+    fraction: Float
+) = layout { measurable, constraints ->
+    val placeable = measurable.measure(constraints)
+
+    val x = -(placeable.width * fraction).roundToInt()
+
+    layout(placeable.width, placeable.height) {
+        placeable.placeRelative(x = x, y = 0)
+    }
+
+}
+
+// Cascade Layout
+@Composable
+fun MainCascadeScreen() {
+    Box {
+        CascadeLayout(spacing = 20) {
+            Box(modifier = Modifier.size(60.dp).background(Color.Blue))
+            Box(modifier = Modifier.size(80.dp, 40.dp).background(Color.Red))
+            Box(modifier = Modifier.size(90.dp, 100.dp).background(Color.Cyan))
+            Box(modifier = Modifier.size(50.dp).background(Color.Magenta))
+            Box(modifier = Modifier.size(70.dp).background(Color.Green))
+        }
+    }
+}
+
+
+// Cascade Layout
+@Composable
+fun CascadeLayout(
+    modifier: Modifier = Modifier,
+    spacing: Int = 0,
+    content: @Composable () -> Unit
+) {
+    Layout(
+        modifier = modifier,
+        content = content
+    ) { measurables, constraints ->
+        var indent = 0
+
+        layout(constraints.maxWidth, constraints.maxHeight) {
+            var yCoord = 0
+
+            val placeables = measurables.map { measurable ->
+                measurable.measure(constraints)
+            }
+
+            placeables.forEach { placeable ->
+                placeable.placeRelative(x = indent, y = yCoord)
+                indent += placeable.width + spacing
+                yCoord += placeable.height + spacing
+            }
+        }
+    }
+}
+
+@Composable
+fun ColorBox(modifier: Modifier) {
+    Box(Modifier.padding(1.dp).size(width = 50.dp, height = 10.dp).then(modifier))
+}
+
+@Composable
+fun MainIntrinsicScreen() {
+
+    var textState by remember { mutableStateOf("") }
+
+    val onTextChange = { text: String ->
+        textState = text
+    }
+
+    Column(Modifier.width(200.dp).padding(5.dp)) {
+        Column(Modifier.width(IntrinsicSize.Min)) {
+            Text(
+                modifier = Modifier
+                    .padding(start = 4.dp),
+                text = textState
+            )
+            Box(Modifier.height(10.dp).fillMaxWidth().background(Color.Blue))
+        }
+        MyTextField(text = textState, onTextChange = onTextChange)
+
+    }
+
+}
+
+@Composable
+fun MyTextField(text: String, onTextChange : (String) -> Unit) {
+
+    TextField(
+        value = text,
+        onValueChange = onTextChange
+    )
+}
+
 @Preview(showBackground = true)
 @Composable
-fun DefaultPreview() {
+fun DefaultPreview1() {
     MaterialTheme {
         MainScreen()
+    }
+}
+
+@Preview(showBackground = true)
+@Composable
+fun DefaultPreview2() {
+    MaterialTheme {
+        MainScreen2()
+    }
+}
+
+@Preview(showBackground = true)
+@Composable
+fun DefaultPreview3() {
+    MaterialTheme {
+        MainBoxScreen()
+    }
+}
+
+@Preview(showBackground = true)
+@Composable
+fun DefaultPreview4() {
+    MaterialTheme {
+        MainCustomScreen()
+    }
+}
+
+@Preview(showBackground = true)
+@Composable
+fun DefaultPreview5() {
+    MaterialTheme {
+        MainCascadeScreen()
     }
 }
