d9f530c192651f318a499b4556ed190ce79594f5
고민균 <97932282+skyblue1232@users.noreply.github.com>
2025-06-23 02:21:58 +0900

    feat/constraint-layout

--- Min/exampleM/app/src/main/java/com/example/exampleM/MainActivity.kt
+++ Min/exampleM/app/src/main/java/com/example/exampleM/MainActivity.kt
@@ -10,19 +10,14 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
-import androidx.compose.foundation.*
-import androidx.compose.foundation.gestures.*
-import androidx.compose.foundation.layout.*
-import androidx.compose.runtime.*
-import androidx.compose.ui.graphics.*
+import androidx.compose.material3.Button
+import androidx.constraintlayout.compose.ConstraintLayout
+import androidx.compose.foundation.layout.size
 import androidx.compose.ui.unit.dp
-import androidx.compose.ui.Alignment
-import androidx.compose.ui.input.pointer.pointerInput
-import androidx.compose.ui.unit.IntOffset
-import androidx.compose.ui.geometry.Offset
-import androidx.compose.ui.res.imageResource
-
-import kotlin.math.roundToInt
+import androidx.compose.foundation.layout.width
+import androidx.constraintlayout.compose.Dimension
+import androidx.compose.ui.unit.Dp
+import androidx.constraintlayout.compose.ConstraintSet
 
 import com.example.exampleM.ui.theme.ExampleMTheme
 
@@ -45,192 +40,56 @@ class MainActivity : ComponentActivity() {
 
 @Composable
 fun MainScreen() {
-    DragDemo()
-}
+    ConstraintLayout(Modifier.size(width = 350.dp, height = 220.dp)) {
+        val (button1, button2, button3) = createRefs()
 
-@Composable
-fun MultiTouchDemo() {
+        val barrier = createEndBarrier(button1, button2)
 
-    var scale by remember { mutableStateOf(1f) }
-    var angle by remember { mutableStateOf(0f) }
-    var offset by remember { mutableStateOf(Offset.Zero)}
+        MyButton(text = "Button1", Modifier.width(100.dp).constrainAs(button1) {
+            top.linkTo(parent.top, margin = 30.dp)
+            start.linkTo(parent.start, margin = 8.dp)
+        })
 
-    val state = rememberTransformableState {
-            scaleChange, offsetChange, rotationChange ->
-        scale *= scaleChange
-        angle += rotationChange
-        offset += offsetChange
-    }
+        MyButton(text = "Button2", Modifier.width(150.dp).constrainAs(button2) {
+            top.linkTo(button1.bottom, margin = 20.dp)
+            start.linkTo(parent.start, margin = 8.dp)
+        })
 
-    Box(contentAlignment = Alignment.Center, modifier = Modifier.fillMaxSize()) {
-        Box(
-            Modifier
-                .graphicsLayer(
-                    scaleX = scale,
-                    scaleY = scale,
-                    rotationZ = angle,
-                    translationX = offset.x,
-                    translationY = offset.y
-
-                )
-                .transformable(state = state)
-                .background(Color.Blue)
-                .size(100.dp)
-        )
+        MyButton(text = "Button3", Modifier.constrainAs(button3) {
+            linkTo(parent.top, parent.bottom,
+                topMargin = 8.dp, bottomMargin = 8.dp)
+            linkTo(button1.end, parent.end, startMargin = 30.dp,
+                endMargin = 8.dp)
+            start.linkTo(barrier, margin = 30.dp)
+            width = Dimension.fillToConstraints
+            height = Dimension.fillToConstraints
+        })
     }
 }
 
-@Composable
-fun ScrollModifiers() {
+private fun myConstraintSet(margin: Dp): ConstraintSet {
+    return ConstraintSet {
+        val button1 = createRefFor("button1")
 
-    val image = ImageBitmap.imageResource(id = R.drawable.vacation)
-
-    Box(modifier = Modifier
-        .size(150.dp)
-        .verticalScroll(rememberScrollState())
-        .horizontalScroll(rememberScrollState())) {
-        Canvas(
-            modifier = Modifier
-                .size(360.dp, 270.dp)
-        )
-        {
-            drawImage(
-                image = image,
-                topLeft = Offset(
-                    x = 0f,
-                    y = 0f
-                ),
-            )
+        constrain(button1) {
+            linkTo(parent.top, parent.bottom, topMargin = margin,
+                bottomMargin = margin)
+            linkTo(parent.start, parent.end, startMargin = margin,
+                endMargin = margin)
+            width = Dimension.fillToConstraints
+            height = Dimension.fillToConstraints
         }
     }
 }
 
 @Composable
-fun ScrollableModifier() {
-
-    var offset by remember { mutableStateOf(0f) }
-
-    Box(
-        Modifier
-            .fillMaxSize()
-            .scrollable(
-                orientation = Orientation.Vertical,
-                state = rememberScrollableState { distance ->
-                    offset += distance
-                    distance
-                }
-            )
+fun MyButton(text: String, modifier: Modifier = Modifier) {
+    Button(
+        onClick = { },
+        modifier = modifier
     ) {
-        Box(modifier = Modifier
-            .size(90.dp)
-            .offset { IntOffset(0, offset.roundToInt()) }
-            .background(Color.Red))
+        Text(text)
     }
-}
-
-@Composable
-fun PointerInputDrag() {
-
-    Box(modifier = Modifier.fillMaxSize()) {
-
-        var xOffset by remember { mutableStateOf(0f) }
-        var yOffset by remember { mutableStateOf(0f) }
-
-        Box(
-            Modifier
-                .offset { IntOffset(xOffset.roundToInt(), yOffset.roundToInt()) }
-                .background(Color.Blue)
-                .size(100.dp)
-                .pointerInput(Unit) {
-                    detectDragGestures { _, distance ->
-                        xOffset += distance.x
-                        yOffset += distance.y
-                    }
-                }
-        )
-    }
-}
-
-@Composable
-fun DragDemo() {
-
-    Box(modifier = Modifier.fillMaxSize()) {
-
-        var xOffset by remember { mutableStateOf(0f) }
-
-        Box(
-            modifier = Modifier
-                .offset { IntOffset(xOffset.roundToInt(), 0) }
-                .size(100.dp)
-                .background(Color.Blue)
-                .draggable(
-                    orientation = Orientation.Horizontal,
-                    state = rememberDraggableState { distance ->
-                        xOffset += distance
-                    }
-                )
-        )
-    }
-}
-
-@Composable
-fun TapPressDemo() {
-
-    var textState by remember {
-        mutableStateOf("Waiting ....")
-    }
-
-    val tapHandler = { status : String ->
-        textState = status
-
-    }
-    Column(
-        horizontalAlignment = Alignment.CenterHorizontally,
-        modifier = Modifier.fillMaxSize()
-    ) {
-        Box(
-            Modifier
-                .padding(10.dp)
-                .background(Color.Blue)
-                .size(100.dp)
-                .pointerInput(Unit) {
-                    detectTapGestures(
-                        onPress = { tapHandler("onPress Detected") },
-                        onDoubleTap = { tapHandler("onDoubleTap Detected") },
-                        onLongPress = { tapHandler("onLongPress Detected") },
-                        onTap = { tapHandler("onTap Detected") }
-                    )
-                }
-        )
-        Spacer(Modifier.height(10.dp))
-        Text(textState)
-    }
-}
-
-@Composable
-fun ClickDemo() {
-
-    var colorState by remember { mutableStateOf(true)}
-    var bgColor by remember { mutableStateOf(Color.Blue) }
-
-    val clickHandler = {
-
-        colorState = !colorState
-
-        if (colorState == true) {
-            bgColor = Color.Blue
-        }
-        else {
-            bgColor = Color.DarkGray
-        }
-    }
-
-    Box(
-        Modifier
-            .clickable { clickHandler() }
-            .background(bgColor)
-            .size(100.dp)
-    )
 }
 
 @Preview(showBackground = true)
