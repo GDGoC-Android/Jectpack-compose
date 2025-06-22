a6948abce461d94ba51f380dddc637f7313e3be9
고민균 <97932282+skyblue1232@users.noreply.github.com>
2025-06-22 21:10:20 +0900

    feat/slot-api

--- Min/exampleM/app/src/main/java/com/example/exampleM/MainActivity.kt
+++ Min/exampleM/app/src/main/java/com/example/exampleM/MainActivity.kt
@@ -4,26 +4,20 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material3.*
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
-import androidx.compose.ui.platform.LocalDensity
-import androidx.compose.ui.unit.dp
-import androidx.compose.foundation.gestures.Orientation
+import androidx.compose.runtime.*
+import androidx.compose.material3.*
 import androidx.compose.foundation.layout.*
-import androidx.compose.foundation.background
-import androidx.compose.foundation.shape.CircleShape
-import androidx.compose.material.ExperimentalMaterialApi
 import androidx.compose.ui.Alignment
-import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.unit.sp
-import androidx.compose.ui.unit.IntOffset
-import androidx.wear.compose.material.ExperimentalWearMaterialApi
-import androidx.wear.compose.material.FractionalThreshold
-import androidx.wear.compose.material.rememberSwipeableState
-import androidx.wear.compose.material.swipeable
-import kotlin.math.roundToInt
+import androidx.compose.ui.unit.dp
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.Image
+import androidx.compose.ui.res.painterResource
 
 import com.example.exampleM.ui.theme.ExampleMTheme
 
@@ -44,75 +38,99 @@ class MainActivity : ComponentActivity() {
     }
 }
 
-@OptIn(ExperimentalMaterialApi::class, ExperimentalWearMaterialApi::class)
 @Composable
 fun MainScreen() {
-    val parentBoxWidth = 320.dp
-    val childBoxSides = 30.dp
 
-    val swipeableState = rememberSwipeableState("L")
-    val widthPx = with(LocalDensity.current) {
-        (parentBoxWidth - childBoxSides).toPx() }
+    var linearSelected by remember { mutableStateOf(true) }
+    var imageSelected by remember { mutableStateOf(true) }
 
-    val anchors = mapOf(0f to "L", widthPx / 2 to "C", widthPx to "R")
+    val onLinearClick = { value : Boolean ->
+        linearSelected = value
+    }
 
-    Box {
-        Box(
-            modifier = Modifier
-                .padding(20.dp)
-                .width(parentBoxWidth)
-                .height(childBoxSides)
-                .swipeable(
-                    state = swipeableState,
-                    anchors = anchors,
-                    thresholds = { _, _ -> FractionalThreshold(0.5f) },
-                    orientation = Orientation.Horizontal
-                )
-        ) {
-            Box(
-                Modifier.fillMaxWidth().height(5.dp).background(Color.DarkGray)
-                    .align(Alignment.CenterStart)
-            )
-            Box(
-                Modifier.size(10.dp).background(
-                    Color.DarkGray,
-                    shape = CircleShape
-                ).align(Alignment.CenterStart)
-            )
-            Box(
-                Modifier.size(10.dp).background(
-                    Color.DarkGray,
-                    shape = CircleShape
-                ).align(Alignment.Center)
-            )
-            Box(
-                Modifier.size(10.dp).background(
-                    Color.DarkGray,
-                    shape = CircleShape
-                ).align(Alignment.CenterEnd)
-            )
+    val onTitleClick = { value : Boolean ->
+        imageSelected = value
+    }
 
-            Box(
-                Modifier
-                    .offset { IntOffset(swipeableState.offset.value.roundToInt(), 0) }
-                    .size(childBoxSides)
-                    .background(Color.Blue),
-                contentAlignment = Alignment.Center
-            ) {
-                Text(
-                    swipeableState.currentValue,
-                    color = Color.White,
-                    fontSize = 22.sp
-                )
+    ScreenContent(
+        linearSelected = linearSelected,
+        imageSelected = imageSelected,
+        onLinearClick = onLinearClick,
+        onTitleClick = onTitleClick,
+        titleContent = {
+            if (imageSelected) {
+
+                TitleImage(drawing = R.drawable.ic_baseline_cloud_download_24)
+
+            } else {
+                Text("Downloading", style = MaterialTheme.typography.headlineMedium,
+                    modifier = Modifier.padding(30.dp))
+            }
+        },
+        progressContent = {
+            if (linearSelected) {
+                LinearProgressIndicator(Modifier.height(40.dp))
+            } else {
+                CircularProgressIndicator(Modifier.size(200.dp),
+                    strokeWidth = 18.dp)
             }
         }
+    )
+
+}
+
+@Composable
+fun ScreenContent(
+    linearSelected: Boolean,
+    imageSelected: Boolean,
+    onTitleClick: (Boolean) -> Unit,
+    onLinearClick: (Boolean) -> Unit,
+    titleContent: @Composable () -> Unit,
+    progressContent: @Composable () -> Unit){
+
+    Column(
+        modifier = Modifier.fillMaxSize(),
+        horizontalAlignment = Alignment.CenterHorizontally,
+        verticalArrangement = Arrangement.SpaceBetween
+    ) {
+        titleContent()
+        progressContent()
+        CheckBoxes(linearSelected, imageSelected, onTitleClick, onLinearClick)
     }
 }
 
-@Preview(showBackground = true)
 @Composable
-fun DefaultPreview() {
-    ExampleMTheme {
-        MainScreen()
+fun CheckBoxes(
+    linearSelected: Boolean,
+    imageSelected: Boolean,
+    onTitleClick: (Boolean) -> Unit,
+    onLinearClick: (Boolean) -> Unit
+) {
+    Row(Modifier.padding(20.dp)) {
+
+        Checkbox(
+            checked = imageSelected,
+            onCheckedChange = onTitleClick
+        )
+        Text("Image Title")
+        Spacer(Modifier.width(20.dp))
+        Checkbox(checked = linearSelected,
+            onCheckedChange = onLinearClick
+        )
+        Text("Linear Progress")
     }
+}
+
+@Composable
+fun TitleImage(drawing: Int) {
+    Image(
+        painter = painterResource(drawing),
+        contentDescription = "title image"
+    )
+}
+
+@Preview(showSystemUi = true)
+@Composable
+fun DemoPreview() {
+    MainScreen()
 }
