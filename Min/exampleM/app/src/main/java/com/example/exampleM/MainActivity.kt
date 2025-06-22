0fd8f021f2e309d0cce9f01a0f9551a539562a98
고민균 <97932282+skyblue1232@users.noreply.github.com>
2025-06-22 21:13:26 +0900

    feat/low-col-demo

--- Min/exampleM/app/src/main/java/com/example/exampleM/MainActivity.kt
+++ Min/exampleM/app/src/main/java/com/example/exampleM/MainActivity.kt
@@ -10,14 +10,15 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
-import androidx.compose.runtime.*
-import androidx.compose.material3.*
+import androidx.compose.foundation.border
+import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.*
-import androidx.compose.ui.Alignment
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.dp
-import androidx.compose.foundation.layout.Row
-import androidx.compose.foundation.Image
-import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.unit.sp
+import androidx.compose.ui.Alignment
 
 import com.example.exampleM.ui.theme.ExampleMTheme
 
@@ -40,97 +41,31 @@ class MainActivity : ComponentActivity() {
 
 @Composable
 fun MainScreen() {
-
-    var linearSelected by remember { mutableStateOf(true) }
-    var imageSelected by remember { mutableStateOf(true) }
-
-    val onLinearClick = { value : Boolean ->
-        linearSelected = value
-    }
-
-    val onTitleClick = { value : Boolean ->
-        imageSelected = value
-    }
-
-    ScreenContent(
-        linearSelected = linearSelected,
-        imageSelected = imageSelected,
-        onLinearClick = onLinearClick,
-        onTitleClick = onTitleClick,
-        titleContent = {
-            if (imageSelected) {
-
-                TitleImage(drawing = R.drawable.ic_baseline_cloud_download_24)
-
-            } else {
-                Text("Downloading", style = MaterialTheme.typography.headlineMedium,
-                    modifier = Modifier.padding(30.dp))
-            }
-        },
-        progressContent = {
-            if (linearSelected) {
-                LinearProgressIndicator(Modifier.height(40.dp))
-            } else {
-                CircularProgressIndicator(Modifier.size(200.dp),
-                    strokeWidth = 18.dp)
-            }
-        }
-    )
-
-}
-
-@Composable
-fun ScreenContent(
-    linearSelected: Boolean,
-    imageSelected: Boolean,
-    onTitleClick: (Boolean) -> Unit,
-    onLinearClick: (Boolean) -> Unit,
-    titleContent: @Composable () -> Unit,
-    progressContent: @Composable () -> Unit){
-
-    Column(
-        modifier = Modifier.fillMaxSize(),
-        horizontalAlignment = Alignment.CenterHorizontally,
-        verticalArrangement = Arrangement.SpaceBetween
-    ) {
-        titleContent()
-        progressContent()
-        CheckBoxes(linearSelected, imageSelected, onTitleClick, onLinearClick)
+    Row {
+        TextCell("1", Modifier.weight(weight = 0.2f, fill = true))
+        TextCell("2", Modifier.weight(weight = 0.4f, fill = true))
+        TextCell("3", Modifier.weight(weight = 0.3f, fill = true))
     }
 }
 
 @Composable
-fun CheckBoxes(
-    linearSelected: Boolean,
-    imageSelected: Boolean,
-    onTitleClick: (Boolean) -> Unit,
-    onLinearClick: (Boolean) -> Unit
-) {
-    Row(Modifier.padding(20.dp)) {
+fun TextCell(text: String, modifier: Modifier = Modifier) {
 
-        Checkbox(
-            checked = imageSelected,
-            onCheckedChange = onTitleClick
-        )
-        Text("Image Title")
-        Spacer(Modifier.width(20.dp))
-        Checkbox(checked = linearSelected,
-            onCheckedChange = onLinearClick
-        )
-        Text("Linear Progress")
+    val cellModifier = Modifier
+        .padding(4.dp)
+        .size(100.dp, 100.dp)
+        .border(width = 4.dp, color = Color.Black)
+
+    Text(text = text, cellModifier.then(modifier),
+        fontSize = 70.sp,
+        fontWeight = FontWeight.Bold,
+        textAlign = TextAlign.Center)
+}
+
+@Preview(showBackground = true)
+@Composable
+fun DefaultPreview() {
+    ExampleMTheme {
+        MainScreen()
     }
-}
-
-@Composable
-fun TitleImage(drawing: Int) {
-    Image(
-        painter = painterResource(drawing),
-        contentDescription = "title image"
-    )
-}
-
-@Preview(showSystemUi = true)
-@Composable
-fun DemoPreview() {
-    MainScreen()
 }
