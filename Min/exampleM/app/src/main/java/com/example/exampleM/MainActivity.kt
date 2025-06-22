9b660c2caf9d15131c13f0b594db908c70498e82
고민균 <97932282+skyblue1232@users.noreply.github.com>
2025-06-23 02:34:16 +0900

    feat/canvas

--- Min/exampleM/app/src/main/java/com/example/exampleM/MainActivity.kt
+++ Min/exampleM/app/src/main/java/com/example/exampleM/MainActivity.kt
@@ -6,17 +6,21 @@ import androidx.activity.compose.setContent
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
-import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
-import androidx.compose.foundation.background
-import androidx.compose.foundation.isSystemInDarkTheme
-import androidx.compose.foundation.layout.Column
-import androidx.compose.runtime.CompositionLocalProvider
-import androidx.compose.runtime.staticCompositionLocalOf
-import androidx.compose.ui.graphics.Color
-import android.content.res.Configuration.UI_MODE_NIGHT_YES
+import androidx.compose.foundation.Canvas
+import androidx.compose.foundation.layout.size
+import androidx.compose.ui.geometry.Offset
+import androidx.compose.ui.geometry.Size
+import androidx.compose.ui.graphics.*
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.graphics.drawscope.Stroke
+import androidx.compose.ui.graphics.drawscope.rotate
+import androidx.compose.ui.res.imageResource
+
+import java.lang.Math.PI
+import java.lang.Math.sin
 
 import com.example.exampleM.ui.theme.ExampleMTheme
 
@@ -30,93 +34,227 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
                 ) {
-                    Composable1()
+                    MainScreen()
                 }
             }
         }
     }
 }
 
-val LocalColor = staticCompositionLocalOf { Color(0xFFffdbcf) }
+@Composable
+fun MainScreen() {
+    DrawImage()
+}
 
 @Composable
-fun Composable1() {
+fun DrawImage() {
 
-    var color = if (isSystemInDarkTheme()) {
-        Color(0xFFa08d87)
-    } else {
-        Color(0xFFffdbcf)
+    val image = ImageBitmap.imageResource(id = R.drawable.vacation)
+
+    Canvas(
+        modifier = Modifier
+            .size(360.dp, 270.dp)
+    ) {
+        drawImage(
+            image = image,
+            topLeft = Offset(x = 0f, y = 0f),
+            colorFilter = ColorFilter.tint(
+                color = Color(0xADFFAA2E),
+                blendMode = BlendMode.ColorBurn
+            )
+        )
     }
+}
 
-    Column {
-        Composable2()
+@Composable
+fun DrawPoints() {
+    Canvas(modifier = Modifier.size(300.dp)) {
 
-        CompositionLocalProvider(LocalColor provides color) {
-            Composable3()
+        val height = size.height
+        val width = size.width
+
+        val points = mutableListOf<Offset>()
+
+        for (x in 0..size.width.toInt()) {
+            val y = (sin(x * (2f * PI /   width))
+                    * (height / 2) + (height / 2)).toFloat()
+            points.add(Offset(x.toFloat(), y))
+        }
+        drawPoints(
+            points = points,
+            strokeWidth = 3f,
+            pointMode = PointMode.Points,
+            color = Color.Blue
+        )
+    }
+}
+
+@Composable
+fun DrawPath() {
+    Canvas(modifier = Modifier.size(300.dp)) {
+
+        val path = Path().apply {
+            moveTo(0f, 0f)
+            quadraticBezierTo(50.dp.toPx(), 200.dp.toPx(),
+                300.dp.toPx(), 300.dp.toPx())
+            lineTo(270.dp.toPx(), 100.dp.toPx())
+            quadraticBezierTo(60.dp.toPx(), 80.dp.toPx(), 0f, 0f)
+            close()
         }
 
+        drawPath(
+            path = path,
+            Color.Blue,
+        )
     }
 }
 
 @Composable
-fun Composable2() {
-    Composable4()
-}
-
-@Composable
-fun Composable3() {
-    Text("Composable 3", modifier = Modifier.background(LocalColor.current))
-
-    CompositionLocalProvider(LocalColor provides Color.Red) {
-        Composable5()
+fun DrawArc() {
+    Canvas(modifier = Modifier.size(300.dp)) {
+        drawArc(
+            Color.Blue,
+            startAngle = 20f,
+            sweepAngle = 90f,
+            useCenter = true,
+            size = Size(250.dp.toPx(), 250.dp.toPx())
+        )
     }
 }
 
 @Composable
-fun Composable4() {
-    Composable6()
-}
+fun ShadowCircle() {
+    Canvas(modifier = Modifier.size(300.dp)) {
+        val radius = 150.dp.toPx()
+        val colorList: List<Color> =
+            listOf(Color.Blue, Color.Black)
 
-@Composable
-fun Composable5() {
-    Text("Composable 5", modifier = Modifier.background(LocalColor.current))
+        val brush = Brush.horizontalGradient(
+            colors = colorList,
+            startX = 0f,
+            endX = 300.dp.toPx(),
+            tileMode = TileMode.Repeated
+        )
 
-    CompositionLocalProvider(LocalColor provides Color.Green) {
-        Composable7()
+        drawCircle(
+            brush = brush,
+            radius = radius
+        )
     }
+}
 
-    CompositionLocalProvider(LocalColor provides Color.Yellow) {
-        Composable8()
+@Composable
+fun RadialFill() {
+    Canvas(modifier = Modifier.size(300.dp)) {
+
+        val canvasWidth = size.width
+        val canvasHeight = size.height
+        val radius = 150.dp.toPx()
+        val colorList: List<Color> = listOf(Color.Red, Color.Blue,
+            Color.Magenta, Color.Yellow, Color.Green, Color.Cyan)
+
+        val brush = Brush.radialGradient(
+            colors = colorList,
+            center = center,
+            radius = radius,
+            tileMode = TileMode.Repeated
+        )
+
+        drawCircle(
+            brush = brush,
+            center = center,
+            radius = radius
+        )
     }
 }
 
 @Composable
-fun Composable6() {
-    Text("Composable 6")
+fun GradientFill() {
+
+    Canvas(modifier = Modifier.size(300.dp)) {
+        val canvasSize = size
+        val colorList: List<Color> = listOf(Color.Red, Color.Blue,
+            Color.Magenta, Color.Yellow, Color.Green, Color.Cyan)
+
+        val brush = Brush.horizontalGradient(
+            colors = colorList,
+            startX = 0f,
+            endX = 300.dp.toPx(),
+            tileMode = TileMode.Repeated
+        )
+
+        drawRect(
+            brush = brush,
+            size = canvasSize
+        )
+    }
 }
 
 @Composable
-fun Composable7() {
-    Text("Composable 7", modifier = Modifier.background(LocalColor.current))
+fun DrawOval() {
+    Canvas(modifier = Modifier.size(300.dp)) {
+        val canvasWidth = size.width
+        val canvasHeight = size.height
+        drawOval(
+            color = Color.Blue,
+            topLeft = Offset(x = 25.dp.toPx(), y = 90.dp.toPx()),
+            size = Size(
+                width = canvasHeight - 50.dp.toPx(),
+                height = canvasHeight / 2 - 50.dp.toPx()
+            ),
+            style = Stroke(width = 12.dp.toPx())
+        )
+    }
 }
 
 @Composable
-fun Composable8() {
-    Text("Composable 8",  modifier = Modifier.background(LocalColor.current))
+fun DrawCircle() {
+    Canvas(modifier = Modifier.size(300.dp)) {
+        val canvasWidth = size.width
+        val canvasHeight = size.height
+
+        drawCircle(
+            color = Color.Blue,
+            center = center,
+            radius = 120.dp.toPx()
+        )
+    }
+}
+
+@Composable
+fun DrawRect() {
+    Canvas(modifier = Modifier.size(300.dp)) {
+        rotate(45f) {
+            drawRect(
+                color = Color.Blue,
+                topLeft = Offset(200f, 200f),
+                size = size / 2f
+            )
+        }
+    }
+}
+
+@Composable
+fun DrawLine() {
+    Canvas(modifier = Modifier.size(300.dp)) {
+        val height = size.height
+        val width = size.width
+
+        drawLine(
+            start = Offset(x= 0f, y = 0f),
+            end = Offset(x = width, y = height),
+            color = Color.Blue,
+            strokeWidth = 16.0f,
+            pathEffect = PathEffect.dashPathEffect(
+                floatArrayOf(30f, 10f, 10f, 10f), phase = 0f)
+        )
+    }
 }
 
 @Preview(showBackground = true)
 @Composable
 fun DefaultPreview() {
     ExampleMTheme {
-        Composable1()
-    }
-}
-
-@Preview(showBackground = true, uiMode = UI_MODE_NIGHT_YES)
-@Composable
-fun DarkPreview() {
-    ExampleMTheme {
-        Composable1()
+        MainScreen()
     }
 }
